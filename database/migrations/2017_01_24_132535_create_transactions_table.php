e49d6b25ed8114d2cd62eb2ddb17b177032f4efe
Martin Bruna <martin.bruna@netbrick.eu>
2017-01-24 14:59:38 +0100

    add transactions and member_transactions tables

---
+++ database/migrations/2017_01_24_132535_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('group_id')->unsigned();
+            $table->integer('value');
+            $table->string('description', 150);
+            $table->timestamps();
+
+            $table->foreign('group_id')->references('id')->on('groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('transactions');
+    }
+}
