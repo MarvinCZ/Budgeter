e07bb0ca3304fcbed1ae7d2fd74b69cf64d2926d
Martin Brom <martinbrom97@gmail.com>
2017-01-24 14:43:22 +0100

    members table migration

---
+++ database/migrations/2017_01_24_134004_create_members_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('group_id')->unsigned();
+            $table->string('name', 50);
+            $table->integer('cached_budget');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->timestamps();
+
+            $table->foreign('group_id')->references('id')->on('groups');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('members');
+    }
+}
