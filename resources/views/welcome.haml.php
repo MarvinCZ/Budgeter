05970b902c72b819a7a85bb83fb88b53ce9bc28e
Martin Bruna <martin.bruna@netbrick.eu>
2017-01-27 17:21:21 +0100

    add layout

--- resources/views/welcome.haml.php
+++ resources/views/welcome.haml.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-%html
-  %head
-    %title Budgeter
-    %link{:rel => "stylesheet", :type => "text/css", :href => "https://fonts.googleapis.com/css?family=Lato:100"}
-    %link{:rel => "stylesheet", :type => "text/css", :href => "bundles/bundle.css"}
-    %link{:rel => "stylesheet", :type => "text/css", :href => "css/app.css"}
-    %script{:type => "text/javascript", :src => "bundles/bundle.js"}
-    %script{:type => "text/javascript", :src => "js/app.js"}
-  %body
-    .center-container
-      .content
-        .title Budgeter
+@extends('layouts.app')
+
+@section('content')
+
+.title Budgeter
+
+@stop
