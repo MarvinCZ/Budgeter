0b3f49e3ebc9241b7aabe59d96a768c9480c3c1d
Martin Bruna <martin.bruna@netbrick.eu>
2017-01-27 18:01:37 +0100

    boost layout page

--- resources/views/layouts/app.haml.php
+++ resources/views/layouts/app.haml.php
@@ -2,12 +2,12 @@
 %html
   %head
     %base{href: URL::asset('/')}
-    %title Budgeter | @yield('title')
-    %link{:rel => "stylesheet", :type => "text/css", :href => "https://fonts.googleapis.com/css?family=Lato:100"}
-    %link{:rel => "stylesheet", :type => "text/css", :href => "bundles/bundle.css"}
-    %link{:rel => "stylesheet", :type => "text/css", :href => "css/app.css"}
-    %script{:type => "text/javascript", :src => "bundles/bundle.js"}
-    %script{:type => "text/javascript", :src => "js/app.js"}
+    %title @yield('title', 'Budgeter')
+    %link{rel: "stylesheet", type: "text/css", href: "https://fonts.googleapis.com/css?family=Lato:100"}
+    %link{rel: "stylesheet", type: "text/css", href: "bundles/bundle.css"}
+    %link{rel: "stylesheet", type: "text/css", href: "css/app.css"}
+    %script{type: "text/javascript", src: "bundles/bundle.js"}
+    %script{type: "text/javascript", src: "js/app.js"}
   %body
     .center-container
       .content
